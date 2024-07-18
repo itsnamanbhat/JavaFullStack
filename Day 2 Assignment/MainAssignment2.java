c67d15f356c570d5262361b186a2d63a4b2296d9
itsnamanbhat <121560844+itsnamanbhat@users.noreply.github.com>
2024-07-18 17:37:17 +0530

    Update MainAssignment2.java

--- Day 2 Assignment/MainAssignment2.java
+++ Day 2 Assignment/MainAssignment2.java
@@ -4,15 +4,15 @@ public class MainAssignment2 {
 public static void main(String[] args) {
 	Items []it=new Items[3];
 	
-	it[0]=new Items("maggie",20);
-	it[1]=new Items("cookies",50);
-	it[2]=new Items("dates",120);
+	it[0]=new Items("mustang",20);
+	it[1]=new Items("lotus",50);
+	it[2]=new Items("mercedes",120);
 	
 	Orders o=new Orders(134,190,"NamanBhat",it);
-	System.out.println("Order Class Object");
+	System.out.println("my Order Class Object");
 	System.out.println(o);
 	
-	System.out.println("Item Class Object");
+	System.out.println("my Item Class Object");
 	for(Items i:it)
 	System.out.println(i);
 	
